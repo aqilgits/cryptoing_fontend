399f9ec221a9a5b1ba98362b5106f74ff02a210c
aqilgits <aqil.dev@gmail.com>
2022-09-21 23:29:03 +0800

    navbar

---
+++ lib/market_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MarketPrediction extends StatefulWidget {
+  const MarketPrediction({Key? key}) : super(key: key);
+
+  @override
+  State<MarketPrediction> createState() => _MarketPredictionState();
+}
+
+class _MarketPredictionState extends State<MarketPrediction> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
