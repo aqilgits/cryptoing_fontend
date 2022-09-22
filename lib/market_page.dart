48f533903d45a8bcfa733293709395bfbfc6bc96
aqilgits <aqil.dev@gmail.com>
2022-09-22 12:27:12 +0800

    draft market prediction page

--- lib/market_page.dart
+++ lib/market_page.dart
@@ -21,7 +21,115 @@ class _MarketPredictionState extends State<MarketPrediction> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("hello market page"),
+      child: Column(
+        children: [
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              height: 70,
+              child: Image.asset(
+                'assets/images/logo.png',
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 30,
+            child: const Center(
+              child: Text(
+                'News',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            decoration: const BoxDecoration(color: Colors.green),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            height: 100,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 30,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Coin',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Text(
+                    "Recommandation",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  )
+                ],
+              ),
+            ),
+            decoration: const BoxDecoration(color: Colors.green),
+          ),
+          Container(
+              padding: const EdgeInsets.all(8),
+              height: 390,
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.orange,
+                  ),
+                ],
+              )),
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+      ),
     );
   }
 }
