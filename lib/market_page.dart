29bab98fd8a3aae9dcf4911cd8faa4f22b49a478
aqilgits <aqil.dev@gmail.com>
2022-09-25 10:21:56 +0800

    latest home screen

--- lib/market_page.dart
+++ lib/market_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
-import 'package:ionicons/ionicons.dart';
 
 class MarketPrediction extends StatefulWidget {
   const MarketPrediction({Key? key}) : super(key: key);
@@ -10,11 +8,8 @@ class MarketPrediction extends StatefulWidget {
 }
 
 class _MarketPredictionState extends State<MarketPrediction> {
-  Widget? _child;
-
   @override
   void initState() {
-    _child = const MarketPrediction();
     super.initState();
   }
 
@@ -54,24 +49,57 @@ class _MarketPredictionState extends State<MarketPrediction> {
               scrollDirection: Axis.horizontal,
               children: <Widget>[
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green),
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                   width: 160.0,
-                  color: Colors.red,
                 ),
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.yellow),
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                   width: 160.0,
-                  color: Colors.blue,
                 ),
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green),
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                   width: 160.0,
-                  color: Colors.green,
                 ),
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red),
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                   width: 160.0,
-                  color: Colors.yellow,
                 ),
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red),
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                   width: 160.0,
-                  color: Colors.orange,
                 ),
               ],
             ),
@@ -80,6 +108,7 @@ class _MarketPredictionState extends State<MarketPrediction> {
             height: 10,
           ),
           Container(
+            padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
             width: MediaQuery.of(context).size.width,
             height: 30,
             child: Center(
@@ -106,24 +135,104 @@ class _MarketPredictionState extends State<MarketPrediction> {
                 padding: const EdgeInsets.all(8),
                 children: <Widget>[
                   Container(
-                    height: 50,
-                    color: Colors.red,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Image.network(
+                              'https://cryptoicons.org/api/color/btc/50'),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    height: 80,
                   ),
                   Container(
-                    height: 50,
-                    color: Colors.blue,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Image.network(
+                              'https://cryptoicons.org/api/color/eth/50'),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    height: 80,
                   ),
                   Container(
-                    height: 50,
-                    color: Colors.green,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Image.network(
+                              'https://cryptoicons.org/api/color/xrp/50'),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    height: 80,
                   ),
                   Container(
-                    height: 50,
-                    color: Colors.yellow,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Image.network(
+                              'https://cryptoicons.org/api/color/ada/50'),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    height: 80,
                   ),
                   Container(
-                    height: 50,
-                    color: Colors.orange,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Image.network(
+                              'https://cryptoicons.org/api/color/bnb/50'),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    height: 80,
                   ),
                 ],
               )),
